7a105326df4962fcb7d6bda8ab4679dee6a384cf
yamini1611 <yaminipriya1611@gmail.com>
2023-04-11 13:18:27 +0530

    final commit

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -1,36 +1,42 @@
+// jquery validation
 $(document).ready(function () {
-    $("#footerform").validate({
-      rules: {
-        username: {
-          required: true,
-          pattern: /^(\w){5,8}$/,
-        },
-        email: {
-          required: true,
-          pattern: /^[\w]+@cgvakindia.com$/
-        },
-        phone: {
-          required: true,
-          pattern: /^(\d){10}$/
-        },
-        comment:{
-          required: true
-        }
-      },
-      username: {
-          required: "This field cant be empty",
-          pattern: "It should contain only alphabet number and underscore",
-        },
-        email: {
-          required: "This field cant be empty",
-          pattern: "It should be cgvak email"
-        },
-        phone: {
-          required:"This field cant be empty",
-          pattern: "must contain 10 numbers only"
-        },
-        comment:{
-          required: "This field cant be empty",
-      },
-    });
-  });
+$(".validate1").validate({
+rules: {
+    'name': {
+        required: true,
+        minlength: 2,
+        maxlength: 15,
+        pattern: /^[A-Za-z ]+$/
+    },
+    'phone': {
+        required: true,
+        minlength: 10,
+        pattern: /^[0-9]{10}$/
+    },
+    'email': {
+        required: true,
+        email: true,
+        pattern: /^[a-z0-9]+@[a-z]+\.[a-z]{2,4}$/
+    }
+},
+messages: {
+    'name': {
+        required: "<br> Please enter a Username.",
+        minlength: "<br> Username is too small",
+        maxlength: "<br> Username is too large",
+        pattern: "<br> Username must contain only alpabets"
+    },
+    'phone': {
+        required: "<br> Please enter  valid Mobile number.",
+        minlength: "<br> Mobile number must contain 10 digits",
+        pattern: "<br> Mobile number must be in the format"
+    },
+
+    'email': {
+        required: "<br> Please enter Email address",
+        email: "<br> Please enter a valid email address",
+        pattern: "<br> E-mail address should be in proper format"
+    }
+}
+});
+});
